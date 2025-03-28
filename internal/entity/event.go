81b40f823f6e3d963f55b8ebd35bc055da931da3
Bruno Bolting <brunoboltingx@gmail.com>
2025-03-28 20:31:33 -0300

    feat: add domain event entity

---
+++ internal/entity/event.go
@@ -0,0 +1,49 @@
+package entity
+
+import (
+	"context"
+	"time"
+)
+
+type EventType string
+
+const (
+	EventCollectionCreated       EventType = "collection:created"
+	EventCollectionUpdated       EventType = "collection:updated"
+	EventCollectionDeleted       EventType = "collection:deleted"
+	EventCollectionSyncFetching  EventType = "collection:sync-fetching"
+	EventCollectionSyncCompleted EventType = "collection:sync-completed"
+	EventCollectionSyncFailed    EventType = "collection:sync-failed"
+)
+
+type Event struct {
+	Type      EventType `json:"type"`
+	Timestamp time.Time `json:"timestamp"`
+	UserID    string    `json:"user_id,omitempty"`
+	Data      any       `json:"data"`
+}
+
+func NewEvent(eventType EventType, userID string, data any) Event {
+	return Event{
+		Type:      eventType,
+		Timestamp: time.Now().UTC(),
+		UserID:    userID,
+		Data:      data,
+	}
+}
+
+type Subscriber struct {
+	SubID  string
+	Ch     chan Event
+	Cancel context.CancelFunc
+	Filter []EventType
+}
+
+type Subscribers map[string]*Subscriber
+
+type EventService interface {
+	Subscribe(subID string, cancel context.CancelFunc, events ...EventType) *Subscriber
+	Unsubscribe(subID string)
+	Publish(event Event)
+	Shutdown(ctx context.Context) error
+}
